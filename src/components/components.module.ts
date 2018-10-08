ebc746c629b3067981954326b5c75b23bfd2a981
Jacques <27236285@student.g.nwu.ac.za>
2018-10-08 17:48:16 +0200

    GUI for Mobile app
    Jacques Vosloo and Tyran Deale Initial GUI

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HelpComponent } from './help/help';
+@NgModule({
+	declarations: [HelpComponent],
+	imports: [],
+	exports: [HelpComponent]
+})
+export class ComponentsModule {}
